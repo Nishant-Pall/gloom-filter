3bdb9b92d803535885b6e56e4363d5e9ab350627
Nishant <palln6935@gmail.com>
2025-04-22 23:23:13 +0530

    configurable hash func slice

--- main.go
+++ main.go
@@ -4,36 +4,45 @@ import (
 	"fmt"
 	"hash/fnv"
 	"hash/maphash"
+	"math/rand/v2"
 )
 
 func main() {
 	len := 40
+	hashes := 5
 
-	gloomFilter, _ := newGloomFilter(len)
+	gloomFilter, _ := newGloomFilter(len, hashes)
 	gloomFilter.addItem("NISHANT")
 	gloomFilter.addItem("ARUSHI")
 
 	fmt.Println(gloomFilter.checkMembership("NISHANT"))
 	fmt.Println(gloomFilter.checkMembership("ARUSHI"))
 
-	// arr := createGloomArr(len)
-	// seed := maphash.MakeSeed()
-
-	// addItem("Nishant", len, arr, seed)
-	// addItem("Arsuhi", len, arr, seed)
-	// fmt.Println(checkMembership("Arsuhi", len, arr, seed))
-
 }
 
-func newGloomFilter(len int) (GloomFilter, error) {
+func newGloomFilter(length int, hashes int) (GloomFilter, error) {
 	var gloomFilter GloomFilter
-	if len < 1 {
+	if length < 1 {
 		return gloomFilter, fmt.Errorf("length cannot be less than 1")
 	}
 
-	gloomFilter.len = len
+	gloomFilter.len = length
 	gloomFilter.createGloomArr()
 	gloomFilter.createSeed()
+
+	gloomFilter.hashes = make([]func(string) uint64, hashes)
+
+	for index := range gloomFilter.hashes {
+
+		// generation should be outside invokation obviously
+		n := rand.Uint64N(100)
+
+		gloomFilter.hashes[index] = func(s string) uint64 {
+			return gloomFilter.mapHash(s) * n
+		}
+	}
+
+	gloomFilter.hashLen = len(gloomFilter.hashes)
 
 	return gloomFilter, nil
 }
@@ -43,6 +52,8 @@ type GloomFilter struct {
 	seed     maphash.Seed
 	len      int
 	hash     maphash.Hash
+	hashes   []func(string) uint64
+	hashLen  int
 }
 
 func (f *GloomFilter) createGloomArr() {
@@ -55,28 +66,23 @@ func (f *GloomFilter) createSeed() {
 
 func (f *GloomFilter) addItem(s string) {
 
-	fmt.Println(f.seed)
-	h1 := f.basicHash(s)
-	h2 := f.mapHash(s)
+	for _, hashFunc := range f.hashes {
+		hashInd := f.modHash(hashFunc(s))
 
-	modHash1 := f.modHash(h1)
-	modHash2 := f.modHash(h2)
-
-	f.gloomArr[modHash1] = 1
-	f.gloomArr[modHash2] = 1
-
+		f.gloomArr[hashInd] = 1
+	}
 }
 
 func (f *GloomFilter) checkMembership(s string) bool {
 
-	fmt.Println(f.seed)
-	h1 := f.basicHash(s)
-	h2 := f.mapHash(s)
+	for _, hashFunc := range f.hashes {
+		hashInd := f.modHash(hashFunc(s))
 
-	modHash1 := f.modHash(h1)
-	modHash2 := f.modHash(h2)
-
-	return bool(f.gloomArr[modHash1] == 1 && f.gloomArr[modHash2] == 1)
+		if f.gloomArr[hashInd] != 1 {
+			return false
+		}
+	}
+	return true
 }
 
 func (f *GloomFilter) mapHash(s string) uint64 {
@@ -98,59 +104,4 @@ func (f *GloomFilter) basicHash(s string) uint64 {
 
 func (f *GloomFilter) modHash(hash uint64) uint64 {
 	return hash % uint64(f.len)
-}
-
-func createGloomArr(len int) []int {
-	arr := make([]int, len)
-	return arr
-}
-
-func hash(s string) uint32 {
-
-	h := fnv.New32a()
-
-	h.Write([]byte(s))
-
-	return h.Sum32()
-}
-
-func mapHash(s string, seed maphash.Seed) uint64 {
-
-	var h maphash.Hash
-	h.SetSeed(seed)
-	h.WriteString(s)
-	str := h.Sum64()
-
-	return str
-
-}
-
-func addItem(item string, len int, gloomArr []int, seed maphash.Seed) {
-
-	// item is hashed through k hash functions
-	h1 := mapHash(item, seed)
-	h2 := hash(item)
-
-	// modulo of n (length of bit array) is executed to identify the k array positions(buckets)
-	mod1 := uint32(h1) % uint32(len)
-	mod2 := uint32(h2) % uint32(len)
-
-	// the bits at all identified buckets are set to one
-	gloomArr[mod1] = 1
-	gloomArr[mod2] = 1
-
-}
-
-func checkMembership(item string, len int, gloomArr []int, seed maphash.Seed) bool {
-
-	// item is hashed through k hash functions
-	h1 := mapHash(item, seed)
-	h2 := hash(item)
-
-	// modulo of n (length of bit array) is executed to identify the k array positions(buckets)
-	mod1 := uint32(h1) % uint32(len)
-	mod2 := uint32(h2) % uint32(len)
-
-	// check if all the bits are set to 1
-	return bool(gloomArr[mod1] == 1 && gloomArr[mod2] == 1)
 }
