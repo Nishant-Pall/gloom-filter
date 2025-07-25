a39f5fc4a0145c1dff862eac0d61d05db27b1ed1
Nishant <palln6935@gmail.com>
2025-07-25 20:57:34 +0530

    instantiate filter through new

--- main.go
+++ main.go
@@ -7,32 +7,42 @@ import (
 	"math/rand/v2"
 )
 
+type GloomFilterHashFunc func(*GloomFilter, string) uint64
+
+func NewGloomFilter() *GloomFilter {
+	return new(GloomFilter)
+}
+
 func main() {
 	len := 40
 	hashes := 5
 
-	gloomFilter, _ := newGloomFilter(len, hashes)
-	gloomFilter.addItem("NISHANT")
-	gloomFilter.addItem("ARUSHI")
+	gloomFilter := NewGloomFilter()
+	gloomFilter.createGloomFilter(len, hashes, MapHash)
 
-	fmt.Println(gloomFilter.checkMembership("NISHANT"))
-	fmt.Println(gloomFilter.checkMembership("ARUSHI"))
+	gloomFilter.AddItem("NISHANT")
+	gloomFilter.AddItem("ARUSHI")
+
+	fmt.Println(gloomFilter.CheckMembership("NISHANT"))
+	fmt.Println(gloomFilter.CheckMembership("ARUSH1"))
 
 }
 
-func newGloomFilter(length int, hashes int) (GloomFilter, error) {
-	var gloomFilter GloomFilter
-	if length < 1 {
-		return gloomFilter, fmt.Errorf("length cannot be less than 1")
-	}
+func MapHash(f *GloomFilter, s string) uint64 {
 
-	gloomFilter.len = length
-	gloomFilter.createGloomArr()
-	gloomFilter.createSeed()
-	gloomFilter.generateHashFunctions(hashes)
-	gloomFilter.hashLen = len(gloomFilter.hashes)
+	f.hash.SetSeed(f.seed)
+	f.hash.WriteString(s)
+	str := f.hash.Sum64()
 
-	return gloomFilter, nil
+	return str
+}
+
+func BasicHash(f *GloomFilter, s string) uint64 {
+
+	h := fnv.New32a()
+	h.Write([]byte(s))
+
+	return uint64(h.Sum32())
 }
 
 type GloomFilter struct {
@@ -44,42 +54,55 @@ type GloomFilter struct {
 	hashLen  int
 }
 
-func (f *GloomFilter) createGloomArr() {
+func (gloomFilter *GloomFilter) createGloomFilter(length int, hashes int, hashFunc GloomFilterHashFunc) error {
+	if length < 1 {
+		return fmt.Errorf("length cannot be less than 1")
+	}
+
+	gloomFilter.len = length
+	gloomFilter.CreateGloomArr()
+	gloomFilter.CreateSeed()
+	gloomFilter.GenerateHashFunctions(hashes, hashFunc)
+	gloomFilter.hashLen = len(gloomFilter.hashes)
+
+	return nil
+}
+
+func (f *GloomFilter) CreateGloomArr() {
 	f.gloomArr = make([]int, f.len)
 }
 
-func (f *GloomFilter) createSeed() {
+func (f *GloomFilter) CreateSeed() {
 	f.seed = maphash.MakeSeed()
 }
 
-func (f *GloomFilter) generateHashFunctions(hashes int) {
+func (f *GloomFilter) GenerateHashFunctions(hashes int, hasFunc GloomFilterHashFunc) {
 	f.hashes = make([]func(string) uint64, hashes)
 
 	for index := range f.hashes {
 
 		// generation should be outside invokation obviously
 		n := rand.Uint64N(100)
-
 		f.hashes[index] = func(s string) uint64 {
-			return f.mapHash(s) * n
+			return MapHash(f, s) * n
 		}
 	}
 
 }
 
-func (f *GloomFilter) addItem(s string) {
+func (f *GloomFilter) AddItem(s string) {
 
 	for _, hashFunc := range f.hashes {
-		hashInd := f.modHash(hashFunc(s))
+		hashInd := f.ModHash(hashFunc(s))
 
 		f.gloomArr[hashInd] = 1
 	}
 }
 
-func (f *GloomFilter) checkMembership(s string) bool {
+func (f *GloomFilter) CheckMembership(s string) bool {
 
 	for _, hashFunc := range f.hashes {
-		hashInd := f.modHash(hashFunc(s))
+		hashInd := f.ModHash(hashFunc(s))
 
 		if f.gloomArr[hashInd] != 1 {
 			return false
@@ -88,23 +111,6 @@ func (f *GloomFilter) checkMembership(s string) bool {
 	return true
 }
 
-func (f *GloomFilter) mapHash(s string) uint64 {
-
-	f.hash.SetSeed(f.seed)
-	f.hash.WriteString(s)
-	str := f.hash.Sum64()
-
-	return str
-}
-
-func (f *GloomFilter) basicHash(s string) uint64 {
-	h := fnv.New32a()
-
-	h.Write([]byte(s))
-
-	return uint64(h.Sum32())
-}
-
-func (f *GloomFilter) modHash(hash uint64) uint64 {
+func (f *GloomFilter) ModHash(hash uint64) uint64 {
 	return hash % uint64(f.len)
 }
