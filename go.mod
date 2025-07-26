968c0f854e957e12cacae89d85da746a973bb154
Nishant <palln6935@gmail.com>
2025-07-26 16:09:11 +0530

    init go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gloom-filter
+
+go 1.22.1
--- main.go
+++ main.go
@@ -3,11 +3,7 @@ package main
 import (
 	"fmt"
 	"hash/fnv"
-	"hash/maphash"
-	"math/rand/v2"
 )
-
-type GloomFilterHashFunc func(*GloomFilter, string) uint64
 
 func NewGloomFilter() *GloomFilter {
 	return new(GloomFilter)
@@ -18,7 +14,7 @@ func main() {
 	hashes := 5
 
 	gloomFilter := NewGloomFilter()
-	gloomFilter.instantiateGloomFilter(len, hashes, MapHash)
+	gloomFilter.InstantiateGloomFilter(len, hashes, MapHash)
 
 	gloomFilter.AddItem("NISHANT")
 	gloomFilter.AddItem("ARUSHI")
@@ -43,74 +39,4 @@ func BasicHash(f *GloomFilter, s string) uint64 {
 	h.Write([]byte(s))
 
 	return uint64(h.Sum32())
-}
-
-type GloomFilter struct {
-	gloomArr []int
-	seed     maphash.Seed
-	len      int
-	hash     maphash.Hash
-	hashes   []func(string) uint64
-	hashLen  int
-}
-
-func (gloomFilter *GloomFilter) instantiateGloomFilter(length int, hashes int, hashFunc GloomFilterHashFunc) error {
-	if length < 1 {
-		return fmt.Errorf("length cannot be less than 1")
-	}
-
-	gloomFilter.len = length
-	gloomFilter.CreateGloomArr()
-	gloomFilter.CreateSeed()
-	gloomFilter.GenerateHashFunctions(hashes, hashFunc)
-	gloomFilter.hashLen = len(gloomFilter.hashes)
-
-	return nil
-}
-
-func (f *GloomFilter) CreateGloomArr() {
-	f.gloomArr = make([]int, f.len)
-}
-
-func (f *GloomFilter) CreateSeed() {
-	f.seed = maphash.MakeSeed()
-}
-
-func (f *GloomFilter) GenerateHashFunctions(hashes int, hasFunc GloomFilterHashFunc) {
-	f.hashes = make([]func(string) uint64, hashes)
-
-	for index := range f.hashes {
-
-		// generation should be outside invokation obviously
-		n := rand.Uint64N(100)
-		f.hashes[index] = func(s string) uint64 {
-			return MapHash(f, s) * n
-		}
-	}
-
-}
-
-func (f *GloomFilter) AddItem(s string) {
-
-	for _, hashFunc := range f.hashes {
-		hashInd := f.ModHash(hashFunc(s))
-
-		f.gloomArr[hashInd] = 1
-	}
-}
-
-func (f *GloomFilter) CheckMembership(s string) bool {
-
-	for _, hashFunc := range f.hashes {
-		hashInd := f.ModHash(hashFunc(s))
-
-		if f.gloomArr[hashInd] != 1 {
-			return false
-		}
-	}
-	return true
-}
-
-func (f *GloomFilter) ModHash(hash uint64) uint64 {
-	return hash % uint64(f.len)
 }
